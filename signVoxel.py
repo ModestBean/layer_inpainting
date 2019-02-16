9bc2718721b38ff5d66c4ba0d01a8797dc865aaa
ModestBean <yindou97@163.com>
2019-02-16 10:11:06 +0800

    六种分类

--- signVoxel.py
+++ signVoxel.py
@@ -6,7 +6,7 @@ import numpy as np
 import imageUtility as utility
 import cv2
 import os
-
+import time
 
 class SkeletonEdge(object):
     """
@@ -52,14 +52,14 @@ def dyeingAndSectionSkeleton(imageStacks, outputDir, ext=".png"):
     label_num = len(imageStacks)
     for item in range(label_num):
         image_label = imageStacks[:, :, item]
-        image_label_num = np.unique(image_label)
         image_rgb = np.zeros((w, h, 3))
-        image_rgb[image_label == 0] = [255, 0, 0]
-        image_rgb[image_label == 1] = [113, 148, 191]
-        image_rgb[image_label == 2] = [100, 87, 63]
-        image_rgb[image_label == 3] = [100, 87, 0]
-        image_rgb[image_label == 4] = [100, 0, 63]
-        image_rgb[image_label == 5] = [0, 87, 63]
+        image_rgb[image_label == 0] = [0, 0, 0]
+        image_rgb[image_label == 1] = [255, 0, 0]
+        image_rgb[image_label == 2] = [0, 255, 0]
+        image_rgb[image_label == 3] = [0, 0, 255]
+        image_rgb[image_label == 4] = [255, 255, 0]
+        image_rgb[image_label == 5] = [0, 255, 255]
+        image_rgb[image_label == 6] = [255, 0, 255]
         cv2.imwrite(os.path.join(outputDir, str(item + 1).zfill(3) + ".png"), image_rgb)
 
 
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     edge_voxel_num = [30, 30, 27, 30, 31, 33]
     edge_num = len(edge_voxel_num)  # 骨架总数
     edgeList = []  # 骨架列表
-    startIndex = 0 # 辅助切割临时变量
+    startIndex = 0  # 辅助切割临时变量
     for i in range(edge_num):
         edgeVoxel = skelPositionStack[startIndex: startIndex+edge_voxel_num[i]]  # 数组切割
         startIndex += edge_voxel_num[i]  # 临时变量叠加
@@ -90,16 +90,18 @@ if __name__ == "__main__":
     # 计算最小距离并设置标记值
     modelVoxelPosition = np.argwhere(modelStack == 1)  # 体素位置
     labelStack = np.zeros((400, 400, 400))  # 初始化空矩阵
-    for everyPosition in modelVoxelPosition[0:30000]:  # [198, 197, 163]
+    startTime = time.time()
+    for everyPosition in modelVoxelPosition:  # [198, 197, 163]
         allEdgeMinDistance = []
         for i in range(len(edgeList)):
             voxelPosition = indexToPosition(edgeList[i].point)
             d = calculateMinDistance(everyPosition, voxelPosition)
             allEdgeMinDistance.append(d)
         minDistance = min(allEdgeMinDistance)
-        edgeIndex = allEdgeMinDistance.index(minDistance)
+        edgeIndex = allEdgeMinDistance.index(minDistance)+1
         labelStack[everyPosition[0], everyPosition[1], everyPosition[2]] = edgeIndex
-
+    endTime = time.time()
+    print("Erode duration:{}'s".format(endTime - startTime))
     # 生成切片
     outputDir = '.\\result\\slicerColor\\'
     dyeingAndSectionSkeleton(labelStack, outputDir)
